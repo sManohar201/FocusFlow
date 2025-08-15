ab100e71b4f01c7c313b34492bee06a1ca5573a8
Sabari Manohar <manoharsabari1@gmail.com>
2025-08-15 11:39:02 +0530

    remove project

--- api/[...slug].ts
+++
@@ -1,5 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handleCatchAll(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(404).json({ message: 'Not found' });
-}
